049aa11ebe5204fe4b74a028cc8ca9a361de01db
Manuel <manuelbustamantealiaga@gmail.com>
2024-08-01 02:50:42 -0400

    new commit

--- heart.py
+++ heart.py
@@ -5,3 +5,14 @@ t = turtle.Turtle()
 t.speed(1)
 t.pensize(5)
 t.color('red', 'pink')  # Color de la línea y el relleno
+
+# Dibujar el corazón
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90, 200)
+t.left(120)
+t.circle(-90, 200)
+t.forward(180)
+t.end_fill()
+
